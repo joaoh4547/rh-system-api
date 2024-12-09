7fbe3b9c39e3e577817ee2222938c418493557f2
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2024-12-09 16:13:25 -0300

    Add security config and enable admin server features
    Introduce a new SecurityConfig class to configure security settings, including disabling CSRF and allowing all CORS origins. Enable admin server and caching in the RhSystemApiApplication to enhance application monitoring and performance. Update application properties to expose management endpoints and configure the admin client URL.

--- src/main/java/com/github/joaoh4547/rhsystem/rhsystemapi/RhSystemApiApplication.java
+++ src/main/java/com/github/joaoh4547/rhsystem/rhsystemapi/RhSystemApiApplication.java
@@ -1,9 +1,13 @@
 package com.github.joaoh4547.rhsystem.rhsystemapi;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableAdminServer
+@EnableCaching
 public class RhSystemApiApplication {
 
     public static void main(String[] args) {

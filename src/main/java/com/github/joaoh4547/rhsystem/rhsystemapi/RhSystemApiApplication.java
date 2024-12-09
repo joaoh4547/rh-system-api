0380865312348b8dda315e2efe9e6f672d10461c
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2024-12-09 09:16:50 -0300

    Initialize Spring Boot project with Gradle configuration.
    This commit sets up a new Spring Boot project named "rh-system-api" with essential Gradle configuration files. It includes basic setups such as build.gradle, settings.gradle, application properties, and wrapper scripts for Gradle. This configuration supports the use of Java 21, various Spring Boot dependencies, and prepares the project for immediate development.

---
+++ src/main/java/com/github/joaoh4547/rhsystem/rhsystemapi/RhSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.joaoh4547.rhsystem.rhsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RhSystemApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/joaoh4547/rhsystem/rhsystemapi/RhSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.joaoh4547.rhsystem.rhsystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
